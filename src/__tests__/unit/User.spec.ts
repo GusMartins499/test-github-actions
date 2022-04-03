c9d46388a7e5858c05df969fea5edcdf492bfad0
Gustavo <gustavosm994@gmail.com>
2022-04-03 02:42:57 -0300

    refactor test

--- src/__tests__/unit/User.spec.ts
+++ src/__tests__/unit/User.spec.ts
@@ -1,36 +1,34 @@
-import { AppError } from "../../erros/AppError";
-import { User } from "../../model/User";
-import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
-import { CreateUserService } from "../../services/users/CreateUserService";
-import { ListUsersService } from "../../services/users/ListUsersService";
+import { AppError } from "../../erros/AppError"
+import { User } from "../../model/User"
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory"
+import { CreateUserService } from "../../services/users/CreateUserService"
 
-let createUserService: CreateUserService;
-let listUserService: ListUsersService;
-let usersRepository: UsersRepositoryInMemory;
+let createUserService: CreateUserService
+let usersRepository: UsersRepositoryInMemory
 
 describe("Create user", () => {
   beforeEach(() => {
-    usersRepository = new UsersRepositoryInMemory();
-    createUserService = new CreateUserService(usersRepository);
-  });
+    usersRepository = new UsersRepositoryInMemory()
+    createUserService = new CreateUserService(usersRepository)
+  })
 
   it("Should be able to create a new user", async () => {
     const user: User = {
       name: "User Example",
       email: "user@example.com",
       password: "123456",
-    };
+    }
 
     await createUserService.execute({
       name: user.name,
       email: user.email,
       password: user.password,
-    });
+    })
 
-    const userCreated = usersRepository.findByEmail(user.email);
+    const userCreated = usersRepository.findByEmail(user.email)
 
-    expect(userCreated).toHaveProperty("id");
-  });
+    expect(userCreated).toHaveProperty("id")
+  })
 
   it("Should not be able to create a new user with same e-mail", async () => {
     expect(async () => {
@@ -38,18 +36,18 @@ describe("Create user", () => {
         name: "User Example",
         email: "user@example.com",
         password: "123456",
-      };
+      }
 
       await createUserService.execute({
         name: user.name,
         email: user.email,
         password: user.password,
-      });
+      })
       await createUserService.execute({
         name: user.name,
         email: user.email,
         password: user.password,
-      });
-    }).rejects.toBeInstanceOf(AppError);
-  });
-});
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
