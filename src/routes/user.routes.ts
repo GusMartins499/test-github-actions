80a077f49d2ad7fbef282e7b612894f57a513ced
Gustavo <gustavosm994@gmail.com>
2022-04-03 02:11:11 -0300

    add eslint

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,29 +1,29 @@
-import { Router } from "express";
-import { UsersRepositoryInMemory } from "../repositories/in-memory/UsersRepositoryInMemory";
+import { Router } from "express"
 
-import { CreateUserService } from "../services/users/CreateUserService";
-import { ListUsersService } from "../services/users/ListUsersService";
+import { UsersRepositoryInMemory } from "../repositories/in-memory/UsersRepositoryInMemory"
+import { CreateUserService } from "../services/users/CreateUserService"
+import { ListUsersService } from "../services/users/ListUsersService"
 
-const usersRoutes = Router();
+const usersRoutes = Router()
 
-const usersRepository = new UsersRepositoryInMemory();
+const usersRepository = new UsersRepositoryInMemory()
 
 usersRoutes.post("/", async (request, response) => {
-  const { name, email, password } = request.body;
+  const { name, email, password } = request.body
 
-  const createUserService = new CreateUserService(usersRepository);
+  const createUserService = new CreateUserService(usersRepository)
 
-  await createUserService.execute({ name, email, password });
+  await createUserService.execute({ name, email, password })
 
-  return response.status(201).send();
-});
+  return response.status(201).send()
+})
 
 usersRoutes.get("/", (request, response) => {
-  const listUsersService = new ListUsersService(usersRepository);
+  const listUsersService = new ListUsersService(usersRepository)
 
   const users = listUsersService.execute()
 
   return response.json(users)
-});
+})
 
-export { usersRoutes };
+export { usersRoutes }
