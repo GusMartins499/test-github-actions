5da8f2dd33417339783c6ccf8507017f3435dc20
Gustavo <gustavosm994@gmail.com>
2022-04-03 18:33:17 -0300

    config pre-commit

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express"
+
+import { AppError } from "../erros/AppError"
+
+export function errorHandler(
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  if (err instanceof AppError) {
+    err as AppError
+
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  })
+}
